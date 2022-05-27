a95c1fbfa10de404cf86999b0f5709c12d6d9948
NathLeem <91312993+NathLeem@users.noreply.github.com>
2022-05-27 08:36:40 +0200

    Add files via upload

---
+++ LeemansNathanJeu5000/LeemansNathanJeu5000/MethodeDuProjet.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeemansNathanJeu5000
+{
+    public struct MethodeDuProjet
+    {
+        public void LancerDes(int[,] tableauDe, Random rnd, int[,] Joueur1, int[,] Joueur2)
+        {
+            for (int i = 0; i < tableauDe.GetLength(1); i++)
+            {
+                Joueur1[0,i] = rnd.Next(1, 7);
+                Joueur2[0,i] = rnd.Next(1,7);
+                tableauDe[0, i] = Joueur1[0,i];
+                tableauDe[1, i] = Joueur2[0,i];
+            }
+        }
+        public void AfficherTableau(int[,] tableauDe, bool J1ouJ2)
+        {
+            if (J1ouJ2 == true)
+            {
+                for (int j = 0; j < tableauDe.GetLength(1); j++)
+                {
+                    Console.Write(tableauDe[0, j]);
+                }    
+            }
+            else
+            {
+                for (int j = 0; j < tableauDe.GetLength(1); j++)
+                {
+                    Console.Write(tableauDe[1, j]);
+                }
+            }        
+            Console.WriteLine();
+        }
+        public void Points(ref int NbrePointJ1, ref int NbrePointJ2, int[,] Joueur1, int[,] Joueur2, int[,] tableauDe)
+        {            
+            if (Joueur1[0, 0] == 5 && Joueur1[0, 1] == 5 && Joueur1[0, 2] == 5 && Joueur1[0, 3] == 5 && Joueur1[0, 4] == 5)
+            {
+                NbrePointJ1 = NbrePointJ1 + 5000;
+            }
+            if (Joueur2[0, 0] == 5 && Joueur2[0, 1] == 5 && Joueur2[0, 2] == 5 && Joueur2[0, 3] == 5 && Joueur2[0, 4] == 5)
+            {
+                NbrePointJ2 = NbrePointJ2 + 5000;
+            }
+            if (Joueur1[0, 0] == 5 || Joueur1[0, 1] == 5 || Joueur1[0, 2] == 5 || Joueur1[0, 3] == 5 || Joueur1[0, 4] == 5)
+            {
+                NbrePointJ1 = NbrePointJ1 + 50;
+            }
+            if (Joueur2[0, 0] == 5 || Joueur2[0, 1] == 5 || Joueur2[0, 2] == 5 || Joueur2[0, 3] == 5 || Joueur2[0, 4] == 5)
+            {
+                NbrePointJ2 = NbrePointJ2 + 50;
+            }
+            if (Joueur1[0, 0] == 1 || Joueur1[0, 1] == 1 || Joueur1[0, 2] == 1 || Joueur1[0, 3] == 1 || Joueur1[0, 4] == 1)
+            {
+                NbrePointJ1 = NbrePointJ1 + 100;
+            }
+            if (Joueur2[0, 0] == 1 || Joueur2[0, 1] == 1 || Joueur2[0, 2] == 1 || Joueur2[0, 3] == 1 || Joueur2[0, 4] == 1)
+            {
+                NbrePointJ2 = NbrePointJ2 + 100;
+            }
+            if (Joueur2[0, 0] != 6 && Joueur2[0, 1] != 6 && Joueur2[0, 2] != 6 && Joueur2[0, 3] != 6 && Joueur2[0, 4] != 6)
+            {
+                if (Joueur2[0,0] != Joueur2[0,1] && Joueur2[0, 0] != Joueur2[0, 2] && Joueur2[0, 0] != Joueur2[0, 3] && Joueur2[0, 0] != Joueur2[0, 4] && Joueur2[0, 1] != Joueur2[0, 2] && Joueur2[0, 1] != Joueur2[0, 3] && Joueur2[0, 1] != Joueur2[0, 4] && Joueur2[0, 2] != Joueur2[0, 3] && Joueur2[0, 2] != Joueur2[0, 4] && Joueur2[0, 3] != Joueur2[0, 4])
+                {
+                    NbrePointJ2 = NbrePointJ2 + 1000;
+                }
+            }
+            if (Joueur1[0, 0] != 6 && Joueur1[0, 1] != 6 && Joueur1[0, 2] != 6 && Joueur1[0, 3] != 6 && Joueur1[0, 4] != 6)
+            {
+                if (Joueur1[0, 0] != Joueur1[0, 1] && Joueur1[0, 0] != Joueur1[0, 2] && Joueur1[0, 0] != Joueur1[0, 3] && Joueur1[0, 0] != Joueur1[0, 4] && Joueur1[0, 1] != Joueur1[0, 2] && Joueur1[0, 1] != Joueur1[0, 3] && Joueur1[0, 1] != Joueur1[0, 4] && Joueur1[0, 2] != Joueur1[0, 3] && Joueur1[0, 2] != Joueur1[0, 4] && Joueur1[0, 3] != Joueur1[0, 4])
+                {
+                    NbrePointJ1 = NbrePointJ1 + 1000;
+                }
+            }
+            if (Joueur2[0, 0] != 1 && Joueur2[0, 1] != 1 && Joueur2[0, 2] != 1 && Joueur2[0, 3] != 1 && Joueur2[0, 4] != 1)
+            {
+                if (Joueur2[0, 0] != Joueur2[0, 1] && Joueur2[0, 0] != Joueur2[0, 2] && Joueur2[0, 0] != Joueur2[0, 3] && Joueur2[0, 0] != Joueur2[0, 4] && Joueur2[0, 1] != Joueur2[0, 2] && Joueur2[0, 1] != Joueur2[0, 3] && Joueur2[0, 1] != Joueur2[0, 4] && Joueur2[0, 2] != Joueur2[0, 3] && Joueur2[0, 2] != Joueur2[0, 4] && Joueur2[0, 3] != Joueur2[0, 4])
+                {
+                    NbrePointJ2 = NbrePointJ2 + 1000;
+                }
+            }
+            if (Joueur1[0, 0] != 1 && Joueur1[0, 1] != 1 && Joueur1[0, 2] != 1 && Joueur1[0, 3] != 1 && Joueur1[0, 4] != 1)
+            {
+                if (Joueur1[0, 0] != Joueur1[0, 1] && Joueur1[0, 0] != Joueur1[0, 2] && Joueur1[0, 0] != Joueur1[0, 3] && Joueur1[0, 0] != Joueur1[0, 4] && Joueur1[0, 1] != Joueur1[0, 2] && Joueur1[0, 1] != Joueur1[0, 3] && Joueur1[0, 1] != Joueur1[0, 4] && Joueur1[0, 2] != Joueur1[0, 3] && Joueur1[0, 2] != Joueur1[0, 4] && Joueur1[0, 3] != Joueur1[0, 4])
+                {
+                    NbrePointJ1 = NbrePointJ1 + 1000;
+                }
+            }
+            if (Joueur1[0,0] == Joueur1[0,1] && Joueur1[0,2] == Joueur1[0,1] || Joueur1[0, 2] == Joueur1[0, 3] && Joueur1[0, 3] == Joueur1[0, 4] || Joueur1[0, 1] == Joueur1[0, 2] && Joueur1[0, 2] == Joueur1[0, 3])
+            {
+                NbrePointJ1 = NbrePointJ1 + 400;
+            }
+            if (Joueur2[0, 0] == Joueur2[0, 1] && Joueur2[0, 2] == Joueur2[0, 1] || Joueur2[0, 2] == Joueur2[0, 3] && Joueur2[0, 3] == Joueur2[0, 4] || Joueur2[0, 1] == Joueur2[0, 2] && Joueur2[0, 2] == Joueur2[0, 3])
+            {
+                NbrePointJ2 = NbrePointJ2 + 400;
+            }
+            if (Joueur1[0,0] == 1 && Joueur1[0,1] == 1 && Joueur1[0, 2] == 1 || Joueur1[0, 2] == 1 && Joueur1[0, 3] == 1 && Joueur1[0, 4] == 1 || Joueur1[0, 1] == 1 && Joueur1[0, 2] == 1 && Joueur1[0, 3] == 1)
+            {
+                NbrePointJ1 = NbrePointJ1 + 600;
+            }
+            if (Joueur2[0, 0] == 1 && Joueur2[0, 1] == 1 && Joueur2[0, 2] == 1 || Joueur2[0, 2] == 1 && Joueur2[0, 3] == 1 && Joueur2[0, 4] == 1 || Joueur2[0, 1] == 1 && Joueur2[0, 2] == 1 && Joueur2[0, 3] == 1)
+            {
+                NbrePointJ2 = NbrePointJ2 + 600;
+            }
+        }
+    }
+}
---
+++ LeemansNathanJeu5000/LeemansNathanJeu5000/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeemansNathanJeu5000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Joueur1 = new int[1,5];
+            int[,] Joueur2 = new int[1,5];
+            int[,] tableauDe = new int[2, 5];
+            Random rnd = new Random();
+            int NbrePointJ1 = 0;
+            int NbrePointJ2 = 0;
+            bool J1ouJ2;
+            string relancer = "oui";
+
+            MethodeDuProjet MesOutils = new MethodeDuProjet();
+
+            Console.WriteLine("-----------");
+            Console.WriteLine("Jeu du 5000");
+            Console.WriteLine("-----------\n");
+
+            while (NbrePointJ1 < 5000 && NbrePointJ2 < 5000)
+            {
+                while (relancer == "oui")
+                {
+                    MesOutils.LancerDes(tableauDe, rnd, Joueur1, Joueur2);
+                    Console.WriteLine("Le premier joueur lance ses dés");
+                    J1ouJ2 = true;
+                    MesOutils.AfficherTableau(tableauDe, J1ouJ2);
+                    Console.WriteLine("Voulez-vous relancer(autoriser une fois)");
+                    relancer = Console.ReadLine();
+                }
+                relancer = "oui";
+                Console.WriteLine("Le deuxième joueur lance ses dés");
+                J1ouJ2 = false;
+                MesOutils.AfficherTableau(tableauDe, J1ouJ2);
+                
+                MesOutils.Points(ref NbrePointJ1, ref NbrePointJ2, Joueur1, Joueur2, tableauDe);
+                
+                Console.WriteLine($"\nScore Joueur 1: \n  {NbrePointJ1}");
+                Console.WriteLine($"Score Joueur 2: \n  {NbrePointJ2}");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            if (NbrePointJ1 >= 5000)
+            {
+                Console.WriteLine("Le jeu est fini, félicitation au joueur N°1");
+            }
+            if (NbrePointJ2 >= 5000)
+            {
+                Console.WriteLine("Le jeu est fini, félicitation au joueur N°2");
+            }
+            Console.ReadLine();
+        }
+    }
+}
